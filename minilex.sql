8bdd4817cbdedc283042e7d9b0bc78f460e8eb86
Vladimir Fomichov <56008690+WaldemarThomson@users.noreply.github.com>
2019-10-08 02:21:13 +0300

    Update minilex.sql
    Создание таблицы и путь к файлу для заполнения.

--- minilex.sql
+++ minilex.sql
@@ -1,5 +1,7 @@
-﻿CREATE TABLE top1000
+CREATE TABLE top1000
 (id smallserial PRIMARY KEY,
 eng VARCHAR (50),
 rus VARCHAR (60),
 score smallint DEFAULT 0);
+# После FROM в одинарных кавычках надо прописать полный путь до файла
+COPY top1000(eng, rus) FROM '../flashcards.csv' WITH (FORMAT csv);
