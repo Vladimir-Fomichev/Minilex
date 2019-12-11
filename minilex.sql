3479b37951be32259298ad7f0e3cb14b063af7d5
Vladimir Fomichev <56008690+Vladimir-Fomichev@users.noreply.github.com>
2019-12-11 04:09:15 +0300

    Delete minilex.sql

--- minilex.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE top1000
-(id smallserial PRIMARY KEY,
-eng VARCHAR (50),
-rus VARCHAR (60),
-score smallint DEFAULT 0);
-# После FROM в одинарных кавычках надо прописать полный путь до файла
-COPY top1000(eng, rus) FROM '../flashcards.csv' WITH (FORMAT csv);
